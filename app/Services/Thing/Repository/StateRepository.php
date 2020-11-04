3654695d3a2e48a4871e78f7834964ce90331cb5
tipine <tipine@163.com>
2020-11-04 23:24:55 +0800

    debug

--- app/Services/Thing/Repository/StateRepository.php
+++ app/Services/Thing/Repository/StateRepository.php
@@ -21,10 +21,10 @@ class StateRepository extends Repository
      * @param array $fields 返回字段
      * @return mixed 状态列表
      */
-    public function getStates(int $thingId, array $fields=null)
+    public function getStates(int $thingId, array $fields = null)
     {
         $states = StateModel::where('thing_id', $thingId)
-            ->when(!empty($fields), function (Builder $query) use ($fields){
+            ->when(!empty($fields), function (Builder $query) use ($fields) {
                 return $query->select($fields);
             })
             ->get();
@@ -98,10 +98,10 @@ class StateRepository extends Repository
      * @param array $fields 返回字段
      * @return mixed 状态条件列表
      */
-    public function getStateConditions(int $stateId, array $fields=null)
+    public function getStateConditions(int $stateId, array $fields = null)
     {
         $conditions = StateConditionModel::where('state_id', $stateId)
-            ->when(!is_null($fields), function (Builder $query) use ($fields){
+            ->when(!is_null($fields), function (Builder $query) use ($fields) {
                 return $query->select($fields);
             })
             ->get();
@@ -115,18 +115,18 @@ class StateRepository extends Repository
      * @param array|null $fields 返回字段
      * @return array|mixed
      */
-    public function getStateConditionsOfThing(int $thingId, array $fields=null)
+    public function getStateConditionsOfThing(int $thingId, array $fields = null)
     {
         $stateConditions = [];
         $states = StateModel::where('thing_id', $thingId)->select(['id'])->get();
-        if(!empty($states)){
+        if (!empty($states)) {
             $stateIds = [];
-            foreach ($states as $state){
+            foreach ($states as $state) {
                 $stateIds[] = $state->id;
             }
 
             $result = StateConditionModel::whereIn('state_id', $stateIds)
-                ->when(!empty($fields), function(Builder $query) use ($fields){
+                ->when(!empty($fields), function (Builder $query) use ($fields) {
                     return $query->select($fields);
                 })
                 ->get();
@@ -149,7 +149,7 @@ class StateRepository extends Repository
      */
     public function addStateCondition(int $stateId, int $fieldId, string $symbol, $value)
     {
-        if(!in_array($symbol, array_keys(StateConditionModel::getSymbolMap()))){
+        if (!in_array($symbol, array_keys(StateConditionModel::getSymbolMap()))) {
             throw new Exception('操作符错误！');
         }
 
@@ -177,38 +177,36 @@ class StateRepository extends Repository
      * @return array|false 成功返回新状态条件，失败返回false
      * @throws Exception
      */
-    public function updateStateCondition(int $conditionId, ?int $stateId=null, ?int $fieldId=null, ?string $symbol=null,
-                                         ?$value=null)
+    public function updateStateCondition(int $conditionId, ?int $stateId = null, ?int $fieldId = null, ?string $symbol = null, ?string $value = null)
     {
-        if(is_null($stateId) && is_null($fieldId) && is_null($symbol) && is_null($value)){
+        if (is_null($stateId) && is_null($fieldId) && is_null($symbol) && is_null($value)) {
             throw new Exception('更新字段不能都为空！');
         }
 
         $condition = StateConditionModel::findOrFail($conditionId);
 
-        if(!is_null($stateId)){
-            if($condition['state_id'] != $stateId){
+        if (!is_null($stateId)) {
+            if ($condition['state_id'] != $stateId) {
                 StateModel::findOrFail($stateId);   // 检查是否存在
                 $condition->state_id = $stateId;
             }
         }
-        if(!is_null($fieldId)){
-            if($condition['field_id'] != $fieldId){
+        if (!is_null($fieldId)) {
+            if ($condition['field_id'] != $fieldId) {
                 FieldModel::findOrFail($fieldId);   // 检查是否存在
                 $condition->field_id = $fieldId;
             }
         }
-        if(!is_null($symbol)){
-            if($condition['symbol'] != $symbol){
-                if(!in_array($symbol, array_keys(StateConditionModel::getSymbolMap()))){
+        if (!is_null($symbol)) {
+            if ($condition['symbol'] != $symbol) {
+                if (!in_array($symbol, array_keys(StateConditionModel::getSymbolMap()))) {
                     throw new Exception('错误的操作符号！');
                 }
                 $condition->symbol = $symbol;
             }
-
         }
-        if(!is_null($value)){
-            if($condition['value'] != $value){
+        if (!is_null($value)) {
+            if ($condition['value'] != $value) {
                 $condition->value = $value;
             }
         }
@@ -267,15 +265,15 @@ class StateRepository extends Repository
      * @return bool|null 是否成功
      * @throws \Exception
      */
-    public function unbindStateAndOperation(?int $stateId=null, ?int $operationId=null)
+    public function unbindStateAndOperation(?int $stateId = null, ?int $operationId = null)
     {
-        if(is_null($stateId) && is_null($operationId)){
+        if (is_null($stateId) && is_null($operationId)) {
             throw new Exception('状态和操作不能都为空！');
         }
-        $result = State2OperationModel::when($stateId, function(Builder $query) use ($stateId){
-                return $query->where('state_id', $stateId);
-            })
-            ->when($operationId, function(Builder $query) use ($operationId){
+        $result = State2OperationModel::when($stateId, function (Builder $query) use ($stateId) {
+            return $query->where('state_id', $stateId);
+        })
+            ->when($operationId, function (Builder $query) use ($operationId) {
                 return $query->where('operation_id', $operationId);
             })
             ->delete();
